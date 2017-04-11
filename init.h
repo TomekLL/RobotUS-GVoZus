45f80bb4cc64c7328df4afe0eec43dacaeec8e10
Kavec1 <matus.kavec@gmail.com>
2017-04-11 16:46:47 +0200

    Create init.h

---
+++ init.h
@@ -0,0 +1,13 @@
+#ifndef init_H_
+#define init_H_
+
+#ifndef F_CPU
+#define F_CPU	10000000UL	//definicia frekvencie CPU
+#endif
+
+#define BAUDERATE 9600
+#define UBRR_VALUE ((F_CPU / (BAUDERATE * 16UL)) -1)  //prenosova rychlost
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
