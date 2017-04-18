238b15b4fe6a2dfa6ec322a735b7da52f986a146
Kavec1 <matus.kavec@gmail.com>
2017-04-18 15:11:11 +0200

    Update plosak_main.c

--- plosak_main.c
+++ plosak_main.c
@@ -20,32 +20,7 @@
  * Author : Filip
  */ 
 
-#include "init.h"
-//kniznica vyuzita na USART komunikaciu
-#include "USART.h"
-
-//definovanie hranicnej hodnoty voltov (nie je x(hodnota) V ale pomer ku 5 voltom) 
-uint16_t LINE_VOLTAGE_MIN;
-//hodnota pozadia
-uint16_t voltageBackground;
-//hodnota popredia
-uint16_t voltageForeground;
-//urcuje ci urcovaci cyklus prebehol(urcovanie voltage)
-bool voltageSet;
-
-//hodnota ktoru skenujeme
-uint16_t checkingValue;
-//hodnota ktoru ideme odoslat
-unsigned char value;
-//ratac cipov
-uint8_t counter;
-//zakladna hodnota admux
-uint8_t admux_base;
-//osvietenie
-bool svieti;
-
-void sendValues();
-uint8_t getBool(uint16_t v);
+#include "plosak.h"
 
 int main(void) {
 	// zapnutie USART spojenia
