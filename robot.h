50d27a75055f8bca760f25928372415db88a1a3e
Kavec1 <matus.kavec@gmail.com>
2017-04-18 18:15:47 +0200

    Update robot.h

--- robot.h
+++ robot.h
@@ -5,3 +5,4 @@
 #include "ultrazvuk.h"
 #define CAS10 255	//kazdych 10 ms bude volana obsluha prerusenia TC0
 #define REZIM 0b00001101 //CTC rezim najpomalsie hodiny F_CPU/1024
+unsigned char speed;
