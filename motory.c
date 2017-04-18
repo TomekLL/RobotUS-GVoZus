3fec1d277e7fd535237c5a2121a19006d8140b99
Kavec1 <matus.kavec@gmail.com>
2017-04-18 12:10:07 +0200

    Update motory.c

--- motory.c
+++ motory.c
@@ -1,1 +1,21 @@
 #include "motory.h"
+
+void nastav()
+{
+	DDRD |=(1<<PD4)|(1<<PD5); 
+	/*motory, USART  PD6= SMER_A, PD7= SMER_B,
+	PD4=RYCHLOST_B, PD5=RYCHLOST_A  */ 
+	DDRB |=(1<<PB4);	//rezim motorov vystup
+	TCCR1A = A_REZIM;
+	TCCR1B = B_REZIM;
+	OCR1AH = 0;			//MOTOR_A stoji
+	OCR1AL = 0;
+	OCR1BH = 0;			//MOTOR_B stoji
+	OCR1BL = 0;
+	PORTD &= ~(1<<PD6);	//nuluj PD6 SMER_A dopredu
+	PORTD &= ~(1<<PD7);	//nuluj PD7 SMER_B dopredu
+	PORTB &= ~(1<<PB4);	//nuluj PB4 REZIM s brzdenim
+	MCUCR |=(1<<ISC00);	// generuje prerusenie na zmenu
+	GICR |=(1<<INT0);	// lokalne povolenie INT0
+	GICR |=(1<<INT1); // lokalne povolenie INT1
+}
