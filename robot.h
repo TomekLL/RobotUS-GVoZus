aa5e13e4fcb56205144c2f741f20df4d55315ba1
MatkoUSKO <pialamar@gmail.com>
2017-04-19 20:26:46 +0200

    uint8_t a nie unit8_t

--- robot.h
+++ robot.h
@@ -6,8 +6,8 @@
 #include "sonar.h"
 #define CAS10 255	//kazdych 10 ms bude volana obsluha prerusenia TC0
 #define REZIM 0b00001101 //CTC rezim najpomalsie hodiny F_CPU/1024
-unit8_t speed;
-unit8_t speedL;
-unit8_t speedR;
+uint8_t speed;
+uint8_t speedL;
+uint8_t speedR;
 unsigned char distance;
 unsigned char hodnota;
